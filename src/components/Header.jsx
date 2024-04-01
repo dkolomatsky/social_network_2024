d79df667aec3d956ade5ea5f4bac1bf4d7fc71bf
Dmytro Kolomatsky <dkolomatsky@gmail.com>
2024-04-01 12:44:03 +0200

    pushed project on Git

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+const Header = () => {
+  return (
+    <header className="header">
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/4/47/PNG_transparency_demonstration_1.png"
+          alt="logo"></img>
+      </header>
+  )
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Profile = () => {
+  return (
+    <div className="content">
+        <div>
+          <img
+            src="https://edwardlowe.org/wp-content/uploads/revslider/mountainpng-1.png"
+            alt="logo"
+          ></img>
+        </div>
+        <div>Ava + Description</div>
+        <div>
+          My Post
+          <div>
+            New post
+            <div>post 1</div>
+            <div>post 2</div>
+          </div>
+        </div>
+    </div>
+  )
+}
+
+export default Profile;
