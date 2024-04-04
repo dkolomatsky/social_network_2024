4f6ddb04924c3fa10f93bdf575f1fbb9df2530c2
Dmytro Kolomatsky <dkolomatsky@gmail.com>
2024-04-04 08:17:17 +0200

    imitated dataBase in dialogs and posts

--- src/components/Navbar/Dialogs/Dialogs.jsx
+++ src/components/Navbar/Dialogs/Dialogs.jsx
@@ -19,19 +19,34 @@ const Message = (props) => {
 }
 
 const Dialog = (props) => {
+
+  let dialogsData = [
+    { id: 1 , name: "User1" },
+    { id: 2 , name: "User2" },
+    { id: 3 , name: "User3" },
+    { id: 4 , name: "User4" },
+    { id: 5 , name: "User5" },
+  ]
+
+  let messagesData = [
+    {message: "message1"},
+    {message: "message2"},
+    {message: "message3"},
+  ]
+
   return (
     <div className={css.dialogs}>
       <div className={css.dialogItems}>
-        <DialogItems name="User1" id="1" />
-        <DialogItems name="User2" id="2" />
-        <DialogItems name="User3" id="3" />
-        <DialogItems name="User4" id="4" />
-        <DialogItems name="User5" id="5" />
+        <DialogItems name={dialogsData[0].name} id={dialogsData[0].id} />
+        <DialogItems name={dialogsData[1].name} id={dialogsData[1].id} />
+        <DialogItems name={dialogsData[2].name} id={dialogsData[2].id} />
+        <DialogItems name={dialogsData[3].name} id={dialogsData[3].id} />
+        <DialogItems name={dialogsData[4].name} id={dialogsData[4].id} />
       </div>
       <div className={css.messages}>
-        <Message message="message1" />
-        <Message message="message2" />
-        <Message message="message3" />
+        <Message message={messagesData[0].message} />
+        <Message message={messagesData[1].message} />
+        <Message message={messagesData[2].message} />
       </div>
     </div>
   )
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -3,17 +3,22 @@ import css from './MyPosts.module.css';
 import Post from "./Post/Post";
 
 const MyPosts = () => {
+
+  let postsData = [
+    {id: 1, post: "Hello, how are you?", count: 3},
+    {id: 2, post: "It's my first post.", count: 10},
+  ];
   return (
     <div className={css.postsBlock}>
           My Post
-          <div>
-            <textarea></textarea>
-            <button>Add post</button>
+          <div className={css.createPostField}>
+            <div><textarea></textarea></div>
+            <div><button>Add post</button></div>
           </div>
           <div className={css}>
             New post:
-          <Post message="Hello, how are you?" count='3'/>
-          <Post message="It's my first post." count='5'/>
+          <Post message={postsData[0].post} count={postsData[0].count}/>
+          <Post message={postsData[1].post} count={postsData[1].count}/>
           </div>
         </div>
   )
